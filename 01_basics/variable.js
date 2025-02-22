abab07503879a14e6b2c2c62dd99946668d84418
Usama Jameel <160001523+UsamaOffical@users.noreply.github.com>
2025-02-22 20:22:50 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,9 @@
+let childName = "Usama"
+childName = "Muhammad Usama"
+
+const fatherName = "Muhammad Jameel"
+// fatherName = "Jameel" constant variable is not update 
+
+console.log("Child Name =",childName,"Father Name =",fatherName)
+
+// var not_use = "best practice Var never use"
